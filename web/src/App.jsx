6e34b5e2afc77899c3bf60c17fe006c205ee2305
Cob007 <godwill2beracah@outlook.com>
2024-03-26 11:56:36 -0400

    Route Structure added

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.scss";
+import Auth from "./routes/Auth/Auth";
+import Main from "./routes/Main/Main";
 
 function App() {
-
   return (
-    <>
-      <h1>Hello Welcome</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<Auth />} />
+        <Route path="/app/*" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
