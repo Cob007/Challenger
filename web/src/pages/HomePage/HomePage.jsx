40c19d6511c71d33c8fb8912d1b292a2e7dcdb0b
Cob007 <godwill2beracah@outlook.com>
2024-04-12 12:36:17 -0400

    Floating Action Button added

--- web/src/pages/HomePage/HomePage.jsx
+++ web/src/pages/HomePage/HomePage.jsx
@@ -1,17 +1,30 @@
-import ChallengeCard from '../../component/ChallengeCard/ChallengeCard';
-import ChallengeList from '../../component/ChallengeList/ChallengeList';
-import Searchbar from '../../component/SearchBar/SearchBar';
-import './HomePage.scss'
+import ChallengeCard from "../../component/ChallengeCard/ChallengeCard";
+import ChallengeList from "../../component/ChallengeList/ChallengeList";
+import FloatingActionButton from "../../component/FloatingActionButton/FloatingActionButton";
+import Searchbar from "../../component/SearchBar/SearchBar";
+import "./HomePage.scss";
 const HomePage = () => {
+  const data = [
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+    { id: 5 },
+    { id: 6 },
+  ];
 
-    const data = [{"id":1},{"id":2},{"id":3},{"id":4},{"id":5},{"id":6}]
+  return (
+    <main className="homepage">
+      <section className="homepage__view">
+        <Searchbar placeholderText={`Find Challenges by title`}/>
+        <ChallengeList data={data} />
+      </section>
 
-    return (
-        <main className='homepage'>
-            <Searchbar />
-            <ChallengeList data={data}/>
-        </main>
-    )
-}
+      <section className="homepage__fab">
+        <FloatingActionButton />
+      </section>
+    </main>
+  );
+};
 
 export default HomePage;
