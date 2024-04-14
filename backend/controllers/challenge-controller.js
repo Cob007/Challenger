f98ff8c2185b934637cd43fd3a51b3aca9a4f9af
Cob007 <godwill2beracah@outlook.com>
2024-04-14 09:34:54 -0400

    Cron job feature added

--- backend/controllers/challenge-controller.js
+++ backend/controllers/challenge-controller.js
@@ -1,28 +1,75 @@
-const {ErrorResponse, SuccessResponse } = require("../constant/Model")
-const MockData = require("../data/mockSample/challenge.json")
+const { CronJob } = require("cron");
+const { ErrorResponse, SuccessResponse } = require("../constant/Model");
+const MockData = require("../data/mockSample/challenge.json");
+const knex = require('knex')(require('../knexfile'))
 
-const getAll = async (_req, res) =>{
-    try {
-        const data = {
-            "data": MockData
-        }
-        res.status(200).json(SuccessResponse(200, data.data, 'Fetched Successfully'))
-    } catch (error) {
-        res.status(404).json(ErrorResponse(400, `Error Occurred!! Caused by ${error}`))
-    }
-}
+const getAll = async (_req, res) => {
+  try {
+    const data = {
+      data: MockData,
+    };
+    res
+      .status(200)
+      .json(SuccessResponse(200, data.data, "Fetched Successfully"));
+  } catch (error) {
+    res
+      .status(404)
+      .json(ErrorResponse(400, `Error Occurred!! Caused by ${error}`));
+  }
+};
 
 const post = async (req, res) => {
-    try {
+  try {
+    const { title, description, contenturl, mediatype, duration } = req.body;
+    console.log(req.email);
+    console.log(req.body);
+    
+    //convert duration to minutes 
+    if (!!title || !!description || !!contenturl || !!mediatype || !!duration) {
         
-    } catch (error) {
-        
+        const userInfo = await knex.select('id').from('user').where({ email: req.email }).first()
+
+        //store to db
+        const result = await knex("challenge").insert({
+            title: title,
+            description: description,
+            contenturl: contenturl,
+            mediatype: mediatype,
+            duration: duration, 
+            status: true,
+            user_id: userInfo.id
+        });
+        console.log(result)
+        const newChallengeId = result[0]
+        const newChallengeObj = await knex("challenge").where({ id: newChallengeId });
+
+        //then create cron job
+            //update challenge status 
+            //write to reward table
+        const date = Date.now();
+        const expireIn = (date + (duration * 60 * 1000));
+        const job = new CronJob(new Date(expireIn),  () => {
+
+            const d = new Date();
+            console.log('Specific date:', date, ', onTick at:', d);
+        });
+
+        //job.start();
+
+      res
+        .status(200)
+        .json(SuccessResponse(200, newChallengeObj , "Fetched Successfully"));
+    } else {
+      res.status(403).json(ErrorResponse(403, "Require field not entered!"));
     }
-}
-
-
+  } catch (error) {
+    res
+      .status(404)
+      .json(ErrorResponse(400, `Error Occurred!! Caused by ${error}`));
+  }
+};
 
 module.exports = {
-    getAll, 
-    post
- }
+  getAll,
+  post,
+};
--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,8 +1,11 @@
 const { ErrorResponse, SuccessResponse } = require("../constant/Model");
 const MockData = require("../data/mockSample/challenge.json");
-const knex = require("knex")(require("./knexfile"));
+const knex = require("knex")(require("../knexfile"));
 const bcrypt = require("bcrypt");
 const { uuid } = require("uuidv4");
+require("dotenv").config();
+const jwt = require('jsonwebtoken')
+
 
 const index = async (_req, res) => {
   try {
@@ -21,26 +24,37 @@ const index = async (_req, res) => {
 
 const register = async (req, res) => {
   try {
+    console.log(req.body);
     const { firstname, lastname, username, email, password } = req.body;
     if (!!firstname || !!lastname || !!username || !!email || !!password) {
       bcrypt.hash(password, 10, async (err, hash) => {
         if (err) throw err;
-        await knex("user").insert({
+        const result = await knex("user").insert({
           firstname: firstname,
           lastname: lastname,
           username: username,
           email: email,
-          password: hash,
-          id: uuid(),
+          password: hash
         });
-      });
-      res
+
+        const newUserId = result[0];
+        console.log(newUserId)
+        const createdUser = await knex("user").where({ id: newUserId });
+        console.log(createdUser)
+
+        const token = jwt.sign({ email }, process.env.SECRET_KEY);
+        const dataRes = {
+          token: token,
+          user: createdUser,
+        };
+
+        res
         .status(201)
-        .json(SuccessResponse(201, data.data, "Fetched Successfully"));
+        .json(SuccessResponse(201, dataRes , "Fetched Successfully"));
+      });
+      
     } else {
-      res
-        .status(403)
-        .json(ErrorResponse(403, "Require field not entered!"));
+      res.status(403).json(ErrorResponse(403, "Require field not entered!"));
     }
   } catch (error) {
     res
@@ -51,11 +65,35 @@ const register = async (req, res) => {
 
 const login = async (req, res) => {
   try {
-    const {email, password} = req.body;
-    
-    res
-      .status(200)
-      .json(SuccessResponse(200, data.data, "Fetched Successfully"));
+    const { email, password } = req.body;
+
+    const userRes = await knex.from("user").where({ email }).first();
+    console.log(userRes);
+    const hash = userRes.password;
+
+    bcrypt.compare(password, hash, (err, result) => {
+      if (err) throw err;
+      if (result) {
+        // Passwords match
+        const token = jwt.sign({ email }, process.env.SECRET_KEY);
+        const dataRes = {
+          token: token,
+          user: {
+            userid: userRes.id,
+            email: userRes.email,
+            firstname: userRes.firstname,
+            lastname: userRes.lastname,
+          },
+        };
+        res
+          .status(200)
+          .json(SuccessResponse(200, dataRes, "Fetched Successfully"));
+      } else {
+        // Passwords do not match
+        console.log("passwords dont match");
+        res.status(404).json(ErrorResponse(404, "Password dont match!"));
+      }
+    });
   } catch (error) {
     res
       .status(404)
