83d627b377d97d6f84124a71288729ef142fc649
Cob007 <godwill2beracah@outlook.com>
2024-04-14 10:18:57 -0400

    Post endpoint logic in progress

--- backend/controllers/post-controller.js
+++ backend/controllers/post-controller.js
@@ -1,28 +1,88 @@
-const {ErrorResponse, SuccessResponse } = require("../constant/Model")
-const MockData = require("../data/mockSample/challenge.json")
+const { ErrorResponse, SuccessResponse } = require("../constant/Model");
+const MockData = require("../data/mockSample/challenge.json");
 
-const getPostByChallengeId = async (_req, res) =>{
-    try {
-        const data = {
-            "data": MockData
-        }
-        res.status(200).json(SuccessResponse(200, data.data, 'Fetched Successfully'))
-    } catch (error) {
-        res.status(404).json(ErrorResponse(400, `Error Occurred!! Caused by ${error}`))
+const getPostByChallengeId = async (_req, res) => {
+  try {
+    const data = {
+      data: MockData,
+    };
+    res
+      .status(200)
+      .json(SuccessResponse(200, data.data, "Fetched Successfully"));
+  } catch (error) {
+    res
+      .status(404)
+      .json(ErrorResponse(400, `Error Occurred!! Caused by ${error}`));
+  }
+};
+
+const get = async (req, res) => {
+  try {
+    const allPost = await knex("post").where({
+      challenge_id: req.params.challengeId,
+    });
+    res.status(200).json(SuccessResponse(200, allPost, "Fetched Successfully"));
+  } catch (error) {
+    res
+      .status(404)
+      .json(ErrorResponse(400, `Error Occurred!! Caused by ${error}`));
+  }
+};
+
+const create = async (req, res) => {
+  try {
+    const { title, posturl, likes } = req.body;
+
+    if (!!title || !!posturl || !!likes) {
+      const userInfo = await knex
+        .select("id")
+        .from("user")
+        .where({ email: req.email })
+        .first();
+
+      //store to db
+      const result = await knex("post").insert({
+        title: title,
+        posturl: posturl,
+        likes: 0,
+        challenge_id: req.params.challengeId,
+        user_id: userInfo.id,
+      });
+      console.log(result);
+      const newPostId = result[0];
+      const newPostObj = await knex("post").where({ id: newPostId });
+
+      res
+        .status(200)
+        .json(SuccessResponse(200, newPostObj, "Fetched Successfully"));
+    } else {
+      res.status(403).json(ErrorResponse(403, "Require field not entered!"));
     }
-}
+  } catch (error) {
+    res
+      .status(404)
+      .json(ErrorResponse(400, `Error Occurred!! Caused by ${error}`));
+  }
+};
 
-const getPostByUserId = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-}
-
-
+const vote = async (_req, res) => {
+  try {
+    const data = {
+      data: MockData,
+    };
+    res
+      .status(200)
+      .json(SuccessResponse(200, data.data, "Fetched Successfully"));
+  } catch (error) {
+    res
+      .status(404)
+      .json(ErrorResponse(400, `Error Occurred!! Caused by ${error}`));
+  }
+};
 
 module.exports = {
-    getPostByChallengeId, 
-    getPostByUserId
- }
+  getPostByChallengeId,
+  get,
+  create,
+  vote,
+};
