a11a3a40fae1246234b66bdddc63a77e38ef4186
Cob007 <godwill2beracah@outlook.com>
2024-04-15 16:44:54 -0400

    Profile Management update added

---
+++ web/src/component/RewardCard/RewardCard.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from 'react'
+import './RewardCard.scss'
+const RewardCard = (props) => {
+    const { data } =props
+    console.log(data)
+    return (
+        <main className='rcard'>
+            <img className='rcard__img' src={data?.posturl} alt='post image url'/>
+            <div className='rcard__body'> 
+                <span className='rcard__span'>You won the 
+                <h3 className='rcard__title'>{data?.title}</h3> 
+                - challenge
+                </span>
+                <p className='rcard__likes'>with {data?.likes} votes </p>
+            </div>
+        </main>
+    )
+}
+
+export default RewardCard
